432aff186db7ca2e56934da14175f7da6c364e98
Valenzuela <sergio@valenzuela.se>
2026-03-01 18:07:11 +0100

    Redirect root URL to /services

---
+++ src/main/java/se/valenzuela/monitoring/ui/RootRedirectView.java
@@ -0,0 +1,14 @@
+package se.valenzuela.monitoring.ui;
+
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class RootRedirectView implements BeforeEnterObserver {
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        event.forwardTo(ServicesPage.class);
+    }
+}
