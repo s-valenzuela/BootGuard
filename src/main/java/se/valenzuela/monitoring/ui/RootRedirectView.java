e743c4df142a2d5ce7d88d4fd4d717dd45ec0a61
Valenzuela <sergio@valenzuela.se>
2026-03-01 18:09:12 +0100

    Fix RootRedirectView to extend Component

--- src/main/java/se/valenzuela/monitoring/ui/RootRedirectView.java
+++ src/main/java/se/valenzuela/monitoring/ui/RootRedirectView.java
@@ -1,11 +1,12 @@
 package se.valenzuela.monitoring.ui;
 
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.Route;
 
 @Route("")
-public class RootRedirectView implements BeforeEnterObserver {
+public class RootRedirectView extends Div implements BeforeEnterObserver {
 
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
